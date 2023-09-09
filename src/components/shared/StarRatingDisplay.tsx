1d6024a21419b0eae5aeb3a2e1bc6b759623f2bd
js1086 <js1086@student.le.ac.uk>
2023-09-09 16:53:50 +0100

    Add interactibility to star rating displays

--- src/components/shared/StarRatingDisplay.tsx
+++ src/components/shared/StarRatingDisplay.tsx
@@ -1,33 +1,93 @@
+"use client";
+
 import styles from "./StarRatingDisplay.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons";
+import { useCallback, useMemo, useState } from "react";
 
 export interface StarRatingDisplayProps {
   rating: number;
+  interactive: boolean;
 }
 
-export default function StarRatingDisplay({ rating }: StarRatingDisplayProps) {
-  const numbers = [0, 1, 2, 3, 4];
-  const overlay = numbers
-    .filter((x) => x < rating)
-    .map((x) => {
-      if (rating - 0.5 === x)
-        return <FontAwesomeIcon icon={faStarHalf} size="sm" />;
-      else return <FontAwesomeIcon icon={faStar} size="sm" />;
-    });
+export default function StarRatingDisplay({
+  rating,
+  interactive = true,
+}: StarRatingDisplayProps) {
+  const [currentRating, setCurrentRating] = useState<number>(rating);
+  const [newRating, setNewRating] = useState<number>(rating);
 
-  const backgroundStars = (
-    <div className={styles.starBackground}>
-      {numbers.map((_, i) => (
-        <FontAwesomeIcon key={i} icon={faStar} size="sm" />
-      ))}
-    </div>
+  const numbers = [0, 1, 2, 3, 4];
+
+  const handleStarHover = useCallback(
+    (
+      event: React.MouseEvent<SVGSVGElement, MouseEvent> | undefined,
+      index: number,
+    ) => {
+      if (!event) {
+        setNewRating(currentRating);
+        return;
+      }
+
+      const starSize =
+        event.currentTarget.getBoundingClientRect().right -
+        event.currentTarget.getBoundingClientRect().left;
+      const offsetX = event.nativeEvent.offsetX;
+
+      const hoveredRating = offsetX / starSize + (index - 1);
+      const roundedRating = Math.round(hoveredRating * 2) / 2;
+
+      setNewRating(roundedRating);
+    },
+    [currentRating],
+  );
+
+  const foregroundStars = useMemo(() => {
+    return numbers
+      .filter((x) => x < newRating)
+      .map((x) => {
+        return (
+          <FontAwesomeIcon
+            key={x}
+            icon={newRating - 0.5 === x ? faStarHalf : faStar}
+            size="sm"
+            style={{ pointerEvents: "none" }}
+          />
+        );
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [newRating]);
+
+  const backgroundStars = useMemo(
+    () => (
+      <div className={styles.starBackground}>
+        {[...Array(5)].map((_, x) => (
+          <FontAwesomeIcon
+            key={x}
+            icon={faStar}
+            size="sm"
+            onMouseMove={
+              interactive ? (e) => handleStarHover(e, x + 1) : undefined
+            }
+            onMouseLeave={
+              interactive
+                ? () => handleStarHover(undefined, currentRating)
+                : undefined
+            }
+          />
+        ))}
+      </div>
+    ),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [currentRating],
   );
 
   return (
     <div className={styles.starRatingDisplay}>
       {backgroundStars}
-      <div className={styles.starForeground}>{overlay}</div>
+      <div className={styles.starForeground} style={{ pointerEvents: "none" }}>
+        {foregroundStars}
+      </div>
     </div>
   );
 }
