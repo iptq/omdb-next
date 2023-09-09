dcabf1ba765352683321894a4fecf34ddce79117
js1086 <js1086@student.le.ac.uk>
2023-09-09 18:35:53 +0100

    Add functionability to `StarRatingDisplay`

--- src/components/shared/StarRatingDisplay.tsx
+++ src/components/shared/StarRatingDisplay.tsx
@@ -4,46 +4,55 @@ import styles from "./StarRatingDisplay.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons";
 import { useCallback, useMemo, useState } from "react";
+import classNames from "classnames";
 
 export interface StarRatingDisplayProps {
   rating: number;
-  interactive: boolean;
+  interactive?: boolean;
+  key?: number;
+  onClick?: (key: number) => void;
 }
 
-export default function StarRatingDisplay({
-  rating,
-  interactive = true,
-}: StarRatingDisplayProps) {
+export default function StarRatingDisplay({ rating, interactive = true, key, onClick }: StarRatingDisplayProps) {
   const [currentRating, setCurrentRating] = useState<number>(rating);
   const [newRating, setNewRating] = useState<number>(rating);
 
-  const numbers = [0, 1, 2, 3, 4];
+  const calculateRatingFromEvent = (event: React.MouseEvent<SVGSVGElement, MouseEvent>, index: number) => {
+    const starSize =
+      event.currentTarget.getBoundingClientRect().right - event.currentTarget.getBoundingClientRect().left;
+    const offsetX = event.nativeEvent.offsetX + 4;
+
+    const hoveredRating = offsetX / starSize + (index - 1);
+    const roundedRating = Math.round(hoveredRating * 2) / 2;
+
+    return roundedRating;
+  };
 
   const handleStarHover = useCallback(
-    (
-      event: React.MouseEvent<SVGSVGElement, MouseEvent> | undefined,
-      index: number,
-    ) => {
+    (event: React.MouseEvent<SVGSVGElement, MouseEvent> | undefined, index: number) => {
       if (!event) {
         setNewRating(currentRating);
         return;
       }
 
-      const starSize =
-        event.currentTarget.getBoundingClientRect().right -
-        event.currentTarget.getBoundingClientRect().left;
-      const offsetX = event.nativeEvent.offsetX;
-
-      const hoveredRating = offsetX / starSize + (index - 1);
-      const roundedRating = Math.round(hoveredRating * 2) / 2;
-
-      setNewRating(roundedRating);
+      const newRating = calculateRatingFromEvent(event, index);
+      setNewRating(newRating);
     },
     [currentRating],
   );
 
+  const handleClick = useCallback((event: React.MouseEvent<SVGSVGElement, MouseEvent>, index: number) => {
+    const newRating = calculateRatingFromEvent(event, index);
+    setCurrentRating(newRating);
+
+    if (key && onClick) {
+      onClick(key);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const foregroundStars = useMemo(() => {
-    return numbers
+    return [0, 1, 2, 3, 4]
       .filter((x) => x < newRating)
       .map((x) => {
         return (
@@ -61,19 +70,14 @@ export default function StarRatingDisplay({
   const backgroundStars = useMemo(
     () => (
       <div className={styles.starBackground}>
-        {[...Array(5)].map((_, x) => (
+        {[0, 1, 2, 3, 4].map((_, x) => (
           <FontAwesomeIcon
             key={x}
             icon={faStar}
             size="sm"
-            onMouseMove={
-              interactive ? (e) => handleStarHover(e, x + 1) : undefined
-            }
-            onMouseLeave={
-              interactive
-                ? () => handleStarHover(undefined, currentRating)
-                : undefined
-            }
+            onClick={interactive ? (e) => handleClick(e, x + 1) : undefined}
+            onMouseMove={interactive ? (e) => handleStarHover(e, x + 1) : undefined}
+            onMouseLeave={interactive ? () => handleStarHover(undefined, currentRating) : undefined}
           />
         ))}
       </div>
@@ -83,7 +87,7 @@ export default function StarRatingDisplay({
   );
 
   return (
-    <div className={styles.starRatingDisplay}>
+    <div className={classNames(styles.starRatingDisplay, { pointer: interactive })}>
       {backgroundStars}
       <div className={styles.starForeground}>{foregroundStars}</div>
     </div>
